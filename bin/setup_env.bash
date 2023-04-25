0ff82e6a7f67a74e21af81027565687757620410
S. V. Paulauskas <stan@xia.com>
2023-04-25 05:57:41 -0400

    fixing remote path to be official repo

--- bin/setup_env.bash
+++ bin/setup_env.bash
@@ -5,4 +5,4 @@ source venv/bin/activate && \
 pip3 install --upgrade pip setuptools wheel && \
 pip3 install -r requirements.txt && \
 pip3 install git+https://github.com/UTA-REST/PyGasMix.git && \
-pip3 install git+https://github.com/xia-stan/PyBoltz.git
+pip3 install git+https://github.com/UTA-REST/PyBoltz.git
